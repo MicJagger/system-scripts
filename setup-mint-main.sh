109601f18a454cfd3e8eaec462163c7d96214b4c
MicJagger <michaeljr623@hotmail.com>
2024-10-29 01:47:35 -0500

    gdb and hexedit

--- setup-mint-main.sh
+++ setup-mint-main.sh
@@ -195,8 +195,10 @@ sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://b
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
 sudo apt install -y brave-browser
+sudo apt install -y gdb
 sudo apt install -y gh
 sudo apt install -y hardinfo
+sudo apt install -y hexedit
 sudo apt install -y hwinfo
 # phoronix test suite
 git clone https://github.com/phoronix-test-suite/phoronix-test-suite.git
